9b43b5ce06ea090c63e2dd7d47c7d8123585314d
SamueldeJeusdeOliveira <samuksjj@gmail.com>
2025-06-24 17:41:08 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeBarAP/Compartilhado/ITela.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeBar.ConsoleApp.Compartilhado
+{
+    public interface ITela
+    {
+        char ApresentarMenu();
+        void CadastrarRegistro();
+        void EditarRegistro();
+        void ExcluirRegistro();
+        void VisualizarRegistros(bool exibirCabecalho);
+
+    }
+}
